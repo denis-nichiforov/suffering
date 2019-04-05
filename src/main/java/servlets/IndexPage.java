c9451dba14bdf2ce42621af2010f3423be4539fa
Денис <salato7@yandex.ru>
2019-04-05 16:33:57 +0300

    Commit 1.0

---
+++ src/main/java/servlets/IndexPage.java
@@ -0,0 +1,62 @@
+package servlets;
+
+import database.MySQLMethods;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+@WebServlet("/main")
+public class IndexPage extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        Integer a = (Integer) session.getAttribute("sessionsId");
+
+
+        if (a == null) {
+            request.getRequestDispatcher("resources/template/mistakes/mistake404.jsp").forward(request, response);
+        } else {
+            String query = "SELECT name FROM date.user where id = '" + a + "'";
+            ResultSet resultSet = null;
+            String name = null;
+
+            try {
+                resultSet = MySQLMethods.executeQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                assert resultSet != null;
+                if (resultSet.next()) {
+
+                    name = resultSet.getString("name");
+                    request.setAttribute("id", a);
+                    request.setAttribute("answer", name); // Will be available as ${products} in JSP
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            request.setAttribute("id", a);
+            request.getRequestDispatcher("/WEB-INF/view/main.jsp").forward(request, response);
+
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+}
